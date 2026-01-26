be42d5e413e229d515085b928800277dc4c1903c
douglas-suczeck-2003 <douglassuczeck@outlook.com>
2026-01-26 17:35:39 -0300

    End of section 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav } from "../layout/nav/nav";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
--- src/learnmore/learnmore.ts
+++ src/learnmore/learnmore.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-learnmore',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './learnmore.html',
   styleUrl: './learnmore.css',
 })
-export class Learnmore {
+export class Learnmore implements OnInit {
+  http = inject(HttpClient);
+  protected members = signal<any>([])
 
+  async ngOnInit() {
+    this.members.set(await this.GetMembers())
+  }
+
+  async GetMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
