63c219fabc3039bd61417868b22719879257a888
douglas-suczeck-2003 <douglassuczeck@outlook.com>
2026-02-03 14:32:30 -0300

    End of section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("register")] // api/account/controller
+    public async Task<ActionResult<UserDto>> Register(RegisterDTO registerDTO)
+    {
+        if (await EmailExists(registerDTO.Email)) return BadRequest("Email taken");
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            DisplayName = registerDTO.DisplayName,
+            Email = registerDTO.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user.ToDto(tokenService);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login([FromBody]LoginDTO loginDTO)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDTO.Email);
+
+        if (user == null) return Unauthorized("Invalid email address");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+        for (var i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return user.ToDto(tokenService);
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+    }
+}
